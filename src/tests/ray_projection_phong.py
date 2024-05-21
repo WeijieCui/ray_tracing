54ba676529a489b283c4f8f2e12a2afb4e615e10
weiji <weijiecui@hotmail.com>
2024-05-21 07:56:08 +0100

    phong picture.

--- src/tests/ray_projection_phong.py
+++ src/tests/ray_projection_phong.py
@@ -29,6 +29,8 @@ class Vector:
 
     def normalize(self):
         length = math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+        if length == 0:
+            return Vector(0, 0, 0)
         return Vector(self.x / length, self.y / length, self.z / length)
 
 
@@ -109,18 +111,71 @@ class Sphere(Shape3D):
             return t, color
 
 
+class Cube(Shape3D):
+    def __init__(self, min_corner, max_corner, color):
+        super().__init__()
+        self.min_corner = min_corner
+        self.max_corner = max_corner
+        self.color = color
+
+    def intersect(self, ray: Ray, scene: Scene) -> (Optional[float], Optional[Vector]):
+        # Compute the intersection of the ray with the cube
+        tmin = 0 if ray.direction.x == 0 else (self.min_corner.x - ray.origin.x) / ray.direction.x
+        tmax = 0 if ray.direction.x == 0 else (self.max_corner.x - ray.origin.x) / ray.direction.x
+
+        if tmin > tmax:
+            tmin, tmax = tmax, tmin
+
+        tymin = 0 if ray.direction.y == 0 else (self.min_corner.y - ray.origin.y) / ray.direction.y
+        tymax = 0 if ray.direction.y == 0 else (self.max_corner.y - ray.origin.y) / ray.direction.y
+
+        if tymin > tymax:
+            tymin, tymax = tymax, tymin
+
+        if (tmin > tymax) or (tymin > tmax):
+            return None, None
+
+        if tymin > tmin:
+            tmin = tymin
+
+        if tymax < tmax:
+            tmax = tymax
+
+        tzmin = 0 if ray.direction.z == 0 else (self.min_corner.z - ray.origin.z) / ray.direction.z
+        tzmax = 0 if ray.direction.z == 0 else (self.max_corner.z - ray.origin.z) / ray.direction.z
+
+        if tzmin > tzmax:
+            tzmin, tzmax = tzmax, tzmin
+
+        if (tmin > tzmax) or (tzmin > tmax):
+            return None, None
+
+        if tzmin > tmin:
+            tmin = tzmin
+
+        if tzmax < tmax:
+            tmax = tzmax
+
+        intersection_point = ray.origin + ray.direction * tmin
+        normal = (intersection_point - self.min_corner).normalize()
+        color = [i * scene.ambient_light for i in self.color]
+        for light in scene.lights:
+            light_dir = (light.position - intersection_point).normalize()
+            for i in range(len(color)):
+                color[i] += light.intensity * self.color[i] * normal.dot(light_dir)
+        return tmin, color
+
+
 def trace(ray, scene: Scene) -> Optional[Shape3D]:
     # Find the closest sphere that the ray intersects
     closest_dist = float('inf')
-    closest_sphere = None
     closest_color = None
-    for sphere in scene.objects:
-        dist, color = sphere.intersect(ray, scene)
+    for shape3d in scene.objects:
+        dist, color = shape3d.intersect(ray, scene)
         if dist is not None:
             # Check if the intersection point is in front of the camera
             if 0 <= dist < closest_dist:
                 closest_dist = dist
-                closest_sphere = sphere
                 closest_color = color
     return closest_color
 
@@ -149,6 +204,13 @@ def test_render():
         Sphere(Vector(20, 0, -120), 10, [1, .5, 0]),
         Sphere(Vector(10, 0, -130), 10, [1, 0, 1]),
         Sphere(Vector(0, 10, -140), 10, [0, 1, 1]),
+        Cube(Vector(-100, -100, -150), Vector(100, 100, -151), [0.5, 0.5, 0]),
+        Cube(Vector(-35, -35, -150), Vector(35, -35, 0), [0.5, 0.5, 0]),
+        Cube(Vector(-35, 35, -150), Vector(35, 35, 0), [0.5, 0.5, 0]),
+        Cube(Vector(-35, -35, -150), Vector(-35, 35, 0), [0.5, 0.5, 0]),
+        Cube(Vector(35, 35, -150), Vector(35, -35, 0), [0.5, 0.5, 0]),
+        Cube(Vector(30, 35, -80), Vector(25, 30, -100), [0.5, 0.5, 0]),
+        Cube(Vector(30, -35, -80), Vector(25, -25, -100), [0.5, 0.5, 0]),
     ]
     lights = [
         Light(Vector(-30, -30, -0), 0.2),
